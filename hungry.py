ad229be5ed4f03271bfb99368fbf2e954ae2961e
Manaswini Kondaveti <manaswinikondaveti@gmail.com>
2025-04-25 14:42:39 -0400

    modified

--- hungry.py
+++ hungry.py
@@ -4,5 +4,7 @@ if answer == 'yes':
     print("eat something")
 elif answer == "i am tired":
     print("then go to sleep")
+elif answer == "i am hungry and also tired":
+    print("Eat something and then sleep")
 else:
     print("do something")
