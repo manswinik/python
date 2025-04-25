28831055c1bf02c8c0e22e966cc7691ae7be3f33
Manaswini Kondaveti <manaswinikondaveti@gmail.com>
2025-04-25 14:38:39 -0400

    Initial commit

---
+++ hungry.py
@@ -0,0 +1,8 @@
+print("are you Hungry")
+answer = str(input("enter your answer"))
+if answer == 'yes':
+    print("eat something")
+elif answer == "i am tired":
+    print("then go to sleep")
+else:
+    print("do something")
