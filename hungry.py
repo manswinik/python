28f61b01bb34bb4a167c489227deb15e86d86517
Manaswini Kondaveti <manaswinikondaveti@gmail.com>
2025-04-26 10:14:29 -0400

    pizza

--- hungry.py
+++ hungry.py
@@ -2,6 +2,10 @@ print("are you Hungry")
 answer = str(input("enter your answer"))
 if answer == 'yes':
     print("eat something")
+    print("what do you want to eat?")
+    an = str(input("enter your answer"))
+    if an == "Pizza":
+        print("eat Pizza")
 elif answer == "i am tired":
     print("then go to sleep")
 elif answer == "i am hungry and also tired":
