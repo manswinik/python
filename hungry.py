22cb0b10a945f440de4af3bd87cf558cff58760d
Manaswini Kondaveti <manaswinikondaveti@gmail.com>
2025-04-26 10:07:43 -0400

    water

--- hungry.py
+++ hungry.py
@@ -6,5 +6,11 @@ elif answer == "i am tired":
     print("then go to sleep")
 elif answer == "i am hungry and also tired":
     print("Eat something and then sleep")
+elif answer == "no":
+    print("Are you thirsty?")
+    ans = str(input("enter your answer"))
+    if ans == "yes":
+        print("Drink some water")
+
 else:
     print("do something")
